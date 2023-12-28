fa5d63565f535b1121e2f870fad4cfd674b76655
Otavio Pascoal <fjoooosa@gmail.com>
2023-12-27 21:33:53 -0300

    deleted:    .gitignore 	deleted:    README.md 	new file:   miniblog/miniblog/package.json 	modified:   miniblog/src/App.js 	modified:   miniblog/src/components/Footer.js 	modified:   miniblog/src/components/Footer.module.css 	modified:   miniblog/src/components/Navbar.js 	modified:   miniblog/src/components/Navbar.module.css 	new file:   miniblog/src/components/PostDetails.js 	new file:   miniblog/src/components/PostDetails.module.css 	new file:   miniblog/src/context/authContext.js 	new file:   miniblog/src/hooks/useAuthenticantion.js 	new file:   miniblog/src/hooks/useDeleteDocument.js 	new file:   miniblog/src/hooks/useFetchDocument.js 	new file:   miniblog/src/hooks/useFetchDocuments.js 	new file:   miniblog/src/hooks/useInsertDocument.js 	new file:   miniblog/src/hooks/useQuerry.js 	new file:   miniblog/src/hooks/useUpdateDocument.js 	modified:   miniblog/src/index.css 	modified:   miniblog/src/pages/About/About.js 	modified:   miniblog/src/pages/About/About.module.css 	new file:   miniblog/src/pages/CreatePost/CreatePost.js 	new file:   miniblog/src/pages/CreatePost/CreatePost.module.css 	new file:   miniblog/src/pages/Dashboard/Dashboard.js 	new file:   miniblog/src/pages/Dashboard/Dashboard.module.css 	new file:   miniblog/src/pages/EditPost/EditPost.js 	new file:   miniblog/src/pages/EditPost/EditPost.module.css 	deleted:    miniblog/src/pages/Home/Home.about.css     modified:   miniblog/src/pages/Home/Home.js 	new file:   miniblog/src/pages/Home/Home.module.css 	new file:   miniblog/src/pages/Login/Login.js 	new file:   miniblog/src/pages/Login/Login.module.css 	new file:   miniblog/src/pages/Post/Post.js 	new file:   miniblog/src/pages/Post/Post.module.css 	new file:   miniblog/src/pages/Register/Register.js 	new file:   miniblog/src/pages/Register/Register.module.css 	new file:   miniblog/src/pages/Search/Search.js 	new file:   miniblog/src/pages/Search/Search.module.css

--- miniblog/src/components/Footer.js
+++ miniblog/src/components/Footer.js
@@ -1,13 +1,12 @@
-//css
-import styles from "./Footer.module.css"
+import styles from "./Footer.module.css";
 
 const Footer = () => {
-    return (
+  return (
     <footer className={styles.footer}>
-            <h3>Escreva sobre o que você tem interesse!</h3>
-            <p>Mini Blog &copy; 2023</p>
-            </footer>
-    );
-}
+      <h3>Escreva sobre o que você tem interesse!</h3>
+      <p>Mini Blog &copy; 2022</p>
+    </footer>
+  );
+};
 
 export default Footer;
